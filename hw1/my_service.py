aae3e1d78d35087a4469135fbc818cf5abc0f82b
Andrei Onea <Andrei.Onea@endava.com>
2019-02-27 18:18:12 +0200

    hw 1

---
+++ hw1/my_service.py
@@ -0,0 +1,53 @@
+import http.server
+from urllib import request
+import json
+import requests
+
+PORT = 1234
+
+Handler = http.server.BaseHTTPRequestHandler
+
+
+class ReqHandler(Handler):
+
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            self.img_url = self.get_image_url()
+            self.imagga_tag = self.get_imagga_tag(self.img_url)
+            content = bytes(
+                self.img_url + ' ' + self.imagga_tag,
+                encoding="UTF-8")
+            self.wfile.write(content)
+
+        return
+
+    def get_image_url(self):
+        response = requests.get("http://www.splashbase.co/api/v1/images/random")
+        print("[my_service] GET completed " + str(response.elapsed))
+        content = json.loads(response.content)
+        image_url = content.get("url")
+
+        return image_url
+
+    def get_imagga_tag(self, url):
+        config = json.load(open("config"))
+
+        api_key = config.get("api_key")
+        api_secret = config.get("api_secret")
+
+        response = requests.get('https://api.imagga.com/v2/tags?image_url=%s' % url,
+                                auth=(api_key, api_secret))
+        print("[my_service] GET completed in " + str(response.elapsed))
+
+        dic = json.loads(response.content)
+        top_prediction_name = dict(dic.get('result').get('tags')[:1][0]).get('tag').get('en')
+        top_prediction_confidence = dict(dic.get('result').get('tags')[:1][0]).get('confidence')
+
+        return top_prediction_name + ' ' + str(top_prediction_confidence)
+
+
+server = http.server.HTTPServer(('', PORT), ReqHandler)
+server.serve_forever()
---
+++ hw1/server.py
@@ -0,0 +1,44 @@
+import http.server
+from urllib import request
+import re
+
+PORT = 8080
+
+Handler = http.server.BaseHTTPRequestHandler
+
+
+class ReqHandler(Handler):
+
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header('Content-Type', 'text/html')
+            self.end_headers()
+            content = bytes(self.make_html(), encoding="UTF-8")
+            self.wfile.write(content)
+
+        return
+
+    def make_html(self):
+        response = re.split(' ', str(request.urlopen("http://localhost:1234").read(), encoding='UTF-8'))
+
+        return '''
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Welcome to page</title>
+            </head>
+            <body>
+                <h1>Hello welcom to page many services good</h1>
+                <ul>
+                    <li>get random image</li>
+                    <li>the internet tells you what it is in the image</li>
+                    <li>????</li>
+                    <li>profit</li>
+                </ul> ''' + "<img src='" + response[0] + "'/>" + "<p>" + response[1] + ' ' + response[
+            2] + "</p>" + "</body></html>"
+
+
+server = http.server.HTTPServer(('', PORT), ReqHandler)
+server.serve_forever()
